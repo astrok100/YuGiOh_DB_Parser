daf6b5fac4eef5439afefc3e3ed9e96b91c06f81
vagrant <vagrant@precise64.(none)>
2017-04-01 16:05:32 +0000

    crawl spider to work with list of cards

--- DB_Parser/DB_Parser/spiders/cardLinkExtractor.py
+++ DB_Parser/DB_Parser/spiders/cardLinkExtractor.py
@@ -7,35 +7,39 @@ class CardLinkExtractor(CrawlSpider):
     name = "cardlink"
     allowed_domains = ["db.yugioh-card.com"]
     start_urls = [
-        'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sort=1&rp=100&page=1',
+        'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sort=1&rp=10&page=1',
     ]
+
+    def process_next_page(value):
+        print value
+        return "/javascript"
+
     rules = [
-        # Rule(
-        #     LxmlLinkExtractor(
-        #         allow=(r'card_search.action.+cid.+'),
-        #         tags='input',
-        #         attrs='value'
-        #     ),
-        #     callback='parse_card'
-        # ),
         Rule(
-            LxmlLinkExtractor(
-                allow=(r'javascript\:Change'),
-                # process_value='process_next_page'
+            LinkExtractor(
+                allow=(r'card_search.action.+cid.+'),
+                tags='input',
+                attrs='value',
+                restrict_css='div[id=article_body] table'
             ),
-            callback='parse_next_page'
-        )
+            callback='parse_card',
+            follow=True
+        ),
+        # Rule(
+        #     LinkExtractor(
+        #         allow=('javascript'),
+        #         process_value=process_next_page,
+        #         restrict_css='div[class=page_num] span a'
+        #     ),
+        #     callback='parse_next_page'
+        # )
     ]
     count =0
     def parse_card(self, response):
         self.count+=1
         print self.count
-        print response
+        print response.xpath('//*[@id="broad_title"]/div/h1/text()').extract()[0].strip()
 
     def parse_next_page(self, response):
-        print "hello"
-
-    def process_next_page(self, value):
-        print "HMPTPTPTPTPTPTPTPPPPPPPP"
-        print value
-        return value
+        self.count+=1
+        print self.count
